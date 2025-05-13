9fe9b78c8a715030d6d06d368d493bcca3ecf28c
PRIYANSHU PATEL <priyanshu.kushwaha.eng@gmail.com>
2025-05-13 14:33:02 +0530

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ProjectHandler-LandingPage/', // 👈 IMPORTANT: replace with your GitHub repo name
+  base: '/ProjectHandler-LandingPage', // 👈 IMPORTANT: replace with your GitHub repo name
 
 })
